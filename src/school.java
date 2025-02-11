c8bccae023d16aa182f6701ef83158cbb4eb4fdf
katumbayi <frankkatumbayi@gmail.com>
2025-02-11 11:47:51 -0800

    commit#6
    In the NV, from line 12 to line changes brought back in the code

--- src/school.java
+++ src/school.java
@@ -9,14 +9,20 @@ public class school{
     public String getDirect(){
         return Direct;
     }
-    
+}
+    class Student{
+    private List<school>schools;
+
+    public Student(){
+        schools = new ArrayList<>();
+    }
     public void addSchool(school s){
         schools.add(s);
     }
     public void displayschools(){
         System.out.println("School List:");
         for(school s: schools){
-            System.out.println("number of schools in the district");
+            System.out.println("number of schools in the distrist");
         }
     }
     }
