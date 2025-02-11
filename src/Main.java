1a2d8dd5b2caebc43da4116aef768d581d81c49c
katumbayi <frankkatumbayi@gmail.com>
2025-02-11 04:58:58 -0800

    commit#2
    In the NV, from line 2 to line 5 changes were made

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+            unix instance = new unix("instance 1");
+            unix instance2 = new unix("instance 2");
+            unix instance3 = new unix("instance 3");
+
+            System.out.println("Instance 1:" + instance.getPlame() + ",lamar" + instance.getLamar());
+            System.out.println("Instance 2:" + instance2.getPlame() + ",lamar" + instance2.getLamar());
+            System.out.println("Instance 3:" + instance3.getPlame() + ",lamar" + instance3.getLamar());
+        }
     }
-}
---
+++ src/unix.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class unix {
+    private static AtomicInteger frank = new AtomicInteger(0);
+    private int plame;
+    private String lamar;
+
+    {
+        plame = frank.incrementAndGet();
+    }
+    public unix(String lamar){
+
+        this.lamar = lamar;
+    }
+    public int getPlame(){
+
+        return plame;
+    }
+    public String getLamar(){
+
+        return lamar;
+    }
+
+}
+
