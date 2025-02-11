70ac92fc3109586ded218d4fab968a356ee7afdc
katumbayi <frankkatumbayi@gmail.com>
2025-02-11 11:12:44 -0800

    commit#1
    Initializing the project

---
+++ src/school.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+public class school{
+    private String Direct;
+
+    public school(String direct){
+        this.Direct = direct;
+    }
+    public String getDirect(){
+        return Direct;
+    }
+}
+    class Student{
+    private List<school>schools;
+
+    public Student(){
+        schools = new ArrayList<>();
+    }
+    public void addSchool(school s){
+        schools.add(s);
+    }
+    public void displayschools(){
+        System.out.println("School List:");
+        for(school s: schools){
+            System.out.println(s);
+        }
+    }
+    }
+
+
