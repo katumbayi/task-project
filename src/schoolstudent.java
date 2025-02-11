39ae5a0def1b9900a2b260b689b6db976006f8ed
katumbayi <frankkatumbayi@gmail.com>
2025-02-11 11:18:55 -0800

    commit#2
    In the NV, from line 3, 9 and 14 changes were made by adding the name kabowa

--- src/schoolstudent.java
+++ src/schoolstudent.java
@@ -1,16 +1,16 @@
 public class schoolstudent {
     public static void main(String[] args) {
-        school school1 = new school("3200");
+        school school1 kabowa = new school("3200");
         school school2 = new school("4000");
         school school3 = new school("2027");
 
         Student student = new Student();
 
-        student.addSchool(school1);
+        student.addSchool(school1 kabowa);
         student.addSchool(school2);
         student.addSchool(school3);
 
         student.displayschools();
-        System.out.println("students in the school " + school1.getDirect());
+        System.out.println("students in the school " + school1 kabowa.getDirect());
     }
 }
