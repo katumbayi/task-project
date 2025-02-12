0588c6645eca3215e0ebb7b2ef2a65a1078c3ab1
katumbayi <frankkatumbayi@gmail.com>
2025-02-12 04:38:44 -0800

    commit#2
    In the NV, from line 4 to line 10 changes were done with the changing of the manager to amount

---
+++ src/enterpriseaccess.java
@@ -0,0 +1,6 @@
+public class enterpriseaccess {
+    public static void main(String[] args) {
+        entriprise company = new entriprise
+
+    }
+}
--- src/entriprise.java
+++ src/entriprise.java
@@ -1,18 +1,27 @@
 public class entriprise {
 
     public String exaucee;
-    int manager;
+    int amount;
     private double remuration;
     String department;
 
     public entriprise(String exaucee, int manager, String department, double remuration) {
         this.exaucee = exaucee;
-        this.manager = manager;
+        this.amount = manager;
         this.department = department;
         this.remuration = remuration;
 
     }
     public double getRemuration() {
         return remuration;
+    }
+    public void displayRemuration() {
+        System.out.println("remuration: " + remuration);
+    }
+    public void display() {
+        System.out.println("exaucee: " + exaucee);
+        System.out.println("manager: " + amount);
+        System.out.println("department: " + department);
+        displayRemuration();
     }
 }
