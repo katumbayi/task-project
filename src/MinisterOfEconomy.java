d8af33f4f40b901f8adf412d762ac43d9ed8891b
katumbayi <frankkatumbayi@gmail.com>
2025-02-18 00:28:36 -0800

    commit#4
    In the NV, from line 7 to line 10 changes were made by the adding of public void display and print "Department"

--- src/MinisterOfEconomy.java
+++ src/MinisterOfEconomy.java
@@ -1,9 +1,12 @@
 public class MinisterOfEconomy extends governmentworkers {
     String department ;
-    
+
     public MinisterOfEconomy( String name , String department, int age ) {
         super( name , age );
         this.department = department;
-
+    }
+    public void display() {
+        super.display();
+        System.out.println("Department : " + department );
     }
 }
