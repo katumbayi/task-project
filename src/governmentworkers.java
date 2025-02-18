548ffed71b4747cfc90fbab21fa48e30258927f8
katumbayi <frankkatumbayi@gmail.com>
2025-02-17 23:22:53 -0800

    commit#1
    this is the project initializer

---
+++ src/governmentworkers.java
@@ -0,0 +1,15 @@
+public class governmentworkers {
+    String name;
+    int age;
+
+    public governmentworkers(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+}
