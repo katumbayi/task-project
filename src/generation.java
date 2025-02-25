bf4b5d8eba79a6eed19238055cce1197f3c634d9
katumbayi <frankkatumbayi@gmail.com>
2025-02-25 00:01:23 -0800

    commit#1
    Initializing the first project

---
+++ src/generation.java
@@ -0,0 +1,12 @@
+public class generation {
+        public void information(){
+            System.out.println("generic information");
+        }
+        public void printinfo(){
+            System.out.println("print default information");
+
+        }
+        public void printinfo(String announcement){
+            System.out.println("print default information2"+ announcement);
+        }
+}
---
+++ src/generationZ.java
@@ -0,0 +1,6 @@
+public class generationZ extends generation {
+    public void information(){
+        System.out.println("This is the generation Z");
+        
+    }
+}
