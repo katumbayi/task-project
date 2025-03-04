2e1ad804e5d99439b94dc2a0afeeac4b9b96441c
katumbayi <frankkatumbayi@gmail.com>
2025-03-04 02:06:44 -0800

    Commit#1
    Initialize the first class

---
+++ task project/src/Company/firstcompany.java
@@ -0,0 +1,17 @@
+package Company;
+
+public class firstcompany {
+    String name;
+
+    public firstcompany(String name){
+        this.name= name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
