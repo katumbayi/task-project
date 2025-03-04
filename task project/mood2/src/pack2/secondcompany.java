9b73b10191e35ea415fa862a981443e18db264f7
katumbayi <frankkatumbayi@gmail.com>
2025-03-04 04:18:22 -0800

    commit#3
    In the NV, the initializetion of the module 2 class

---
+++ task project/mood2/src/pack2/secondcompany.java
@@ -0,0 +1,17 @@
+package pack2;
+
+public class secondcompany {
+    double price;
+
+    public secondcompany(double price){
+    this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
