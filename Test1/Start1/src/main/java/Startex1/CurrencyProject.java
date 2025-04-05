36694aa8670f45be061d2320b33ee3d883598743
katumbayi <frankkatumbayi@gmail.com>
2025-04-05 02:12:56 -0700

    Commit#6
    In the NV, on line on line 4 private Startproject was added

--- Test1/Start1/src/main/java/Startex1/CurrencyProject.java
+++ Test1/Start1/src/main/java/Startex1/CurrencyProject.java
@@ -1,5 +1,14 @@
 package Startex1;
-
+import
 public class CurrencyProject {
-    private 
+    private StartProject;
+
+    public CurrencyProject(StartProject){
+        this.StartProject= Startproject;
+
+    }
+    public double convert(double amount,String fromCurrencyRate,String toCurrencyRate){
+        double rate= StartProject.getRate(fromCurrencyRate,toCurrencyRate);
+        return amount * rate;
+    }
 }
