0f88b97edf2c7f38a86dd8db237b7e5fc7a159e4
katumbayi <frankkatumbayi@gmail.com>
2025-04-06 02:34:19 -0700

    Commit#2
    the first class iniitialization

---
+++ Data1/src/HospitalService.java
@@ -0,0 +1,10 @@
+public class HospitalService implements Service{
+     String Department;
+     int ID;
+
+     public HospitalService(String Department,int ID){
+         this.Department = Department;
+         this.ID = ID;
+     }
+
+}
