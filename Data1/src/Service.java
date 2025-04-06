3a0fd1a5b3c5cdea40fa84b8f13bd02234fbc4d8
katumbayi <frankkatumbayi@gmail.com>
2025-04-06 02:25:07 -0700

    Commit#1
    Project Initialization

---
+++ Data1/src/Service.java
@@ -0,0 +1,3 @@
+public interface Service {
+    String Hospital(String source);
+}
--- Test1/Start1/src/test/java/Startex1/CurrencyProjectTest.java
+++ Test1/Start1/src/test/java/Startex1/CurrencyProjectTest.java
@@ -6,21 +6,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CurrencyProjectTest {
-
+     CurrencyProject cost3;
     @BeforeEach
     void setUp() {
-
+       cost3 = new CurrencyProject("30",3000);
     }
 
     @Test
     void getItermCost() {
-        double price = 50000;
-        assertEquals(price,getItermCost(30000);
+        double profit = 500000;
+        assertEquals(profit,cost3.GetItermCost(500));
 
     }
 
     @Test
     void conert() {
-
+         double profit = 100;
+         assertEquals(profit, cost3.conert(3000,"USD","EUR"));
     }
 }
